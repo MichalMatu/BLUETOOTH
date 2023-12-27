e2b2d52da60d478debf7ae1f1719df0a8eb083bb
MichalMatu <michal.matuszewski1984@gmail.com>
2023-12-27 00:13:32 +0000

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
-#include <Arduino.h>
+// This example code is in the Public Domain (or CC0 licensed, at your option.)
+// By Evandro Copercini - 2018
+//
+// This example creates a bridge between Serial and Classical Bluetooth (SPP)
+// and also demonstrate that SerialBT have the same functionalities of a normal Serial
 
-// put function declarations here:
-int myFunction(int, int);
+#include "BluetoothSerial.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup()
+{
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); // Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  if (Serial.available())
+  {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available())
+  {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
 }
